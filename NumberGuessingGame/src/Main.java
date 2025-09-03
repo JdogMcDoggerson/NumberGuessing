57e7bb30a7a52968246890ea6d513dce333a09e6
McDoggerson <135794957+JdogMcDoggerson@users.noreply.github.com>
2025-09-03 13:02:52 -0500

    add basic if-else to determine if the player guess is correct

--- NumberGuessingGame/src/Main.java
+++ NumberGuessingGame/src/Main.java
@@ -13,8 +13,23 @@ public class Main {
 
     System.out.println("I am thinking of a whole number number between 1 and 100. Try to guess it.");
     System.out.print("Input a whole number, then press enter:");
-    playerCurrentGuess = scnr.nextInt();
+    playerCurrentGuess = scnr.nextInt();Math.random();
+
+    if (programChosenNum == playerCurrentGuess) {
+        System.out.println("Correct! the number I was thinking of is indeed " + playerCurrentGuess + ".");
+    }
+    else {
+        System.out.print("Incorrect. The number I'm thinking of is ");
+    }
+    if (playerCurrentGuess < programChosenNum) {
+        System.out.print("higher than " + playerCurrentGuess + ".");
+    }
+    else if (playerCurrentGuess > programChosenNum) {
+        System.out.print("lower than " + playerCurrentGuess + ".");
+
+    }
+    System.out.println(" Program's number: " + programChosenNum); //TEST. REMOVE LATER
 
 
-        }
+    }
     }
