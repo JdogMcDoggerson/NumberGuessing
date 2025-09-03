4bbc319f635c72016a66cfb7b59e87d6d5ee2836
McDoggerson <135794957+JdogMcDoggerson@users.noreply.github.com>
2025-09-03 12:41:02 -0500

    import scanner, add initial variables

--- NumberGuessingGame/src/Main.java
+++ NumberGuessingGame/src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        int programChosenNum = 0;
+        int playerCurrentGuess = 0;
+        int numOfPlayerGuesses = 0;
+
     System.out.println("I am thinking of a number between 1 and 100. Try to guess it.");
+
         }
     }
